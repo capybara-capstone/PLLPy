a641e145daf2efb8c3642506dbcd5a3e1c6e0700
Nanik <nanik@pop-os.localdomain>
2024-09-30 17:30:14 -0400

    fixed adding and integral stage, added time_step and time_stop functionality

--- PLL/VCO.py
+++ PLL/VCO.py
@@ -14,18 +14,24 @@ import matplotlib.pyplot as plt
 VDD = 1
 VSS = 0
 default_frq = 1e6
+k_vco = 1e6
+
+#given in seconds
+time_step = 1e-9
+stop_time = 1e-5
 
 def block_1_a(input_voltage):
-    return (input_voltage * 2 * math.pi)
+    return (input_voltage * 2 * math.pi * k_vco)
 
 def block_1_b(default_frequency):
-    return  default_frequency * 2 * math.pi
+    return  (default_frequency * 2 * math.pi)
 
 def block_2(input_voltage, angular_frequency):
-    return (input_voltage * angular_frequency)
+    return (input_voltage + angular_frequency)
 
 def block_3(new_value, prev_value):
-    return (new_value + prev_value)
+    integral_result = prev_value + (new_value * time_step)
+    return (integral_result)
 
 def block_4(integral):
     return math.cos(integral)
@@ -39,8 +45,8 @@ def block_5(sine_out):
 
 def VCO(a, o, frq):
     total_integral = 0;
-    for i in range(0, 100):
-        new_voltage = block_1_a(a[i]);
+    for i in a:
+        new_voltage = block_1_a(i);
         angular_frq = block_1_b(frq)
 
         multiply = block_2(new_voltage, angular_frq)
@@ -51,9 +57,11 @@ def VCO(a, o, frq):
 # Test?
 a = []
 o = []
+number_of_elements = math.floor(stop_time / time_step)
 
-for i in range(0, 100):
-    a.append(math.sin(i/10))
+for i in range(0, number_of_elements):
+    a.append(1+math.sin(i/math.floor(number_of_elements/10)))
+    #a.append(1)
 
 VCO(a, o, default_frq)
 
@@ -61,7 +69,7 @@ original_signal = np.array(a)
 output_VCO = np.array(o)
 fig, axs = plt.subplots(2)
 axs[0].plot(original_signal,color="red")
-axs[1].step(np.array(range(0, 100)),output_VCO,color="blue")
+axs[1].step(np.array(range(0, number_of_elements)),output_VCO,color="blue")
 
 plt.show()
 
