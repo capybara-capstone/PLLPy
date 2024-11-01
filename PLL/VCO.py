1c225cb9401bdb5e06456200c1b0a66e646da44a
Jennifer Mah <jennifermah76@gmail.com>
2024-11-01 18:29:16 -0400

    Add VCO tests, add a seperate VCO example that makes a pop-up plot

--- PLL/VCO.py
+++ PLL/VCO.py
@@ -8,24 +8,28 @@
 ####
 
 import math
-import numpy as np
-import matplotlib.pyplot as plt
 
-VDD = 1
-VSS = 0
-default_frq = 1e6
+VDD = 0
+VSS = 1
+default_frq = 1e9
+k_vco = 1e9
+
+#given in seconds
+time_step = 1e-11
+stop_time = 3e-11
 
 def block_1_a(input_voltage):
-    return (input_voltage * 2 * math.pi)
+    return (input_voltage * 2 * math.pi * k_vco)
 
 def block_1_b(default_frequency):
-    return  default_frequency * 2 * math.pi
+    return  (default_frequency * 2 * math.pi)
 
 def block_2(input_voltage, angular_frequency):
-    return (input_voltage * angular_frequency)
+    return (input_voltage + angular_frequency)
 
 def block_3(new_value, prev_value):
-    return (new_value + prev_value)
+    integral_result = prev_value + (new_value * time_step)
+    return (integral_result)
 
 def block_4(integral):
     return math.cos(integral)
@@ -39,32 +43,11 @@ def block_5(sine_out):
 
 def VCO(a, o, frq):
     total_integral = 0;
-    for i in range(0, 100):
-        new_voltage = block_1_a(a[i]);
+    for i in a:
+        new_voltage = block_1_a(i);
         angular_frq = block_1_b(frq)
 
         multiply = block_2(new_voltage, angular_frq)
         total_integral = block_3(multiply, total_integral)
         sin_out = block_4(total_integral)
         o.append(block_5(sin_out))
-
-# Test?
-a = []
-o = []
-
-for i in range(0, 100):
-    a.append(math.sin(i/10))
-
-VCO(a, o, default_frq)
-
-original_signal = np.array(a)
-output_VCO = np.array(o)
-fig, axs = plt.subplots(2)
-axs[0].plot(original_signal,color="red")
-axs[1].step(np.array(range(0, 100)),output_VCO,color="blue")
-
-plt.show()
-
-
-
-
