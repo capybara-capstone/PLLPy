d712f9f7928286104738ed2999896379551019df
Jennifer Mah <52462750+jmah4@users.noreply.github.com>
2024-11-17 20:42:36 +0000

    Add the divider matplotlib example, add __pycache__ to the gitignore (#7)
    Reformat pfd loop filter files
    
    Co-authored-by: Nanik <nanik@pop-os.localdomain>

--- tests/pfdLoopFilter_test.py
+++ tests/pfdLoopFilter_test.py
@@ -4,21 +4,9 @@ from tests.utils import *
 
 tests_data_dir = os.path.dirname(os.path.abspath(__file__))
 
-#def test_sin_example():
-#    df = read_test_csv(f"{os.path.join(tests_data_dir, 'vco_3.csv')}")
-#     
-#    output_array = []
-#    VCO(df.v_in, output_array, 1e9)
-#
-#    assert (verify_output(df.out, output_array, 0.01) == True)
-#
-
 def test_conly():
     fields = ["time","out","in1","in2"]
     df = read_test_csv(fields, f"{os.path.join(tests_data_dir, 'C_only.csv')}")
-#    df = pd.read_csv(f"{os.path.join(tests_data_dir, 'C_only.csv')}", skipinitialspace=True, usecols=["time","out","in1","in2"]) 
-
-
 
     output_array = []
     output_array.append(0)
@@ -32,8 +20,6 @@ def test_conly():
     axs[2].plot(output_array)
     axs[3].plot(df.out)
     plt.show()
-
     '''
-    
 
     assert(verify_output(df.out, output_array, 0.01) == True)
