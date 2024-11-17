7446cc30bb3d6eee794ba5661be80ea15b6bfa0c
Jennifer Mah <52462750+jmah4@users.noreply.github.com>
2024-11-17 20:42:36 +0000

    Add the divider matplotlib example, add __pycache__ to the gitignore (#7)
    Reformat pfd loop filter files
    
    Co-authored-by: Nanik <nanik@pop-os.localdomain>

--- PLL/divider.py
+++ PLL/divider.py
@@ -5,22 +5,11 @@ Created on Sat Nov 16 13:17:24 2024
 @author: Sharmieka
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-import math
-
-time_step = 1e-9
-stop_time = 1e-5
-
 # power and ground
 VDD = 1
 VSS = 0
 
-# Divider ratio
-N = 0
-
-
-def div(a, b, o, VDD, VSS, number_of_elements, N):
+def div(a, o, VDD, VSS, number_of_elements, N):
     
     counter_up = 0
     counter_down = 0
@@ -71,29 +60,3 @@ def div(a, b, o, VDD, VSS, number_of_elements, N):
             else: o.append(VSS) #continue
     return o
 
-# TEST
-
-a = []
-b = []
-o = []
-
-number_of_elements = math.floor(stop_time / time_step)
-
-# discretized input square wave
-for i in range(0, number_of_elements):
-
-    if (1+math.sin(i/math.floor(number_of_elements/30)))>1:
-
-        a.append(VDD)
-    else:
-        a.append(VSS)
-    
-o = div(a, b, o, VDD, VSS, number_of_elements, N)
-
-original_signal = np.array(a)
-divided_signal = np.array(o)
-
-fig, axs = plt.subplots(2)
-axs[0].plot(original_signal,color="pink")
-axs[1].plot(divided_signal,color="blue")
-plt.show()
