f0f8531863ff9897ff66c38e9972e88b99f8278d
nanik <nanikgeorge@gmail.com>
2025-03-23 21:24:50 -0400

    put back run_sim.py

---
+++ run_sim.py
@@ -0,0 +1,13 @@
+"""Sim """
+from components.pll import Pll
+from utils.settings import Settings
+
+settings = Settings()
+
+# pll = Pll(settings=settings)
+# pll.start()
+
+pll = Pll(settings=settings)
+pll.start_and_monitor()
+pll.show(plot_type=settings.pll['plot_mode'])
+pll.save_to_file("./logs/")
