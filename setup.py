18c36949ef89e9f3d1d355a449972b7a2bfa9c99
Jimmy Velasco <jimmy.velascogranda@mail.utoronto.ca>
2025-03-25 20:10:52 -0400

    name changed to pllpython

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pll_py',
+    name='pllpython',
     version='0.0.1',
     packages=find_packages(),
     install_requires=['bokeh == 3.6.3',
